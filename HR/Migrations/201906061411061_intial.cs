892cac86b4f98f7da6f77b1463d1bfccf44690de
khrv <khrv1695@gmail.com>
2019-06-06 21:14:48 +0530

    project adding

---
+++ HR/Migrations/201906061411061_intial.cs
@@ -0,0 +1,26 @@
+namespace HR.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class intial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Departments",
+                c => new
+                    {
+                        Id = c.Int(nullable: false, identity: true),
+                        Name = c.String(),
+                    })
+                .PrimaryKey(t => t.Id);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Departments");
+        }
+    }
+}
